25ca9aee0141489256e6d61b39ecf45f157c7643
= <joerusselldavis@kubrickgroup.com>
2023-06-12 14:40:44 +0100

    Completed First Streamlit tasks

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,4 +16,14 @@ sl.text('🥑🍞 Avocado Toast')
 sl.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-sl.dataframe(my_fruit_list)
+
+#lets put a pick list here so they can pick the fruit they want to include
+
+my_fruit_list = my_fruit_list.set_index('Fruit')
+fruits_selected = sl.multiselect('Pick some fruits:', list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+#display the table on the page
+
+sl.dataframe(fruits_to_show)
+
